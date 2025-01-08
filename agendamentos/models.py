ab69bd37a836b1fc60cdf16258593a42951c12ac
Morais <moraisgabriel923@gmail.com>
2025-01-08 20:08:03 -0300

    saving

--- agendamentos/models.py
+++ agendamentos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from clientes.models import Cliente
 
-# Create your models here.
+class Agendamento(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='agendamentos')
+    tipo_corte = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    data_horario = models.DateTimeField()
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('pendente', 'Pendente'),
+            ('confirmado', 'Confirmado'),
+            ('cancelado', 'Cancelado')
+        ],
+        default='pendente'
+    )
+
+    def __str__(self):
+        return f"{self.cliente.nome} - {self.data_horario.strftime('%d/%m/%Y %H:%M')}"
