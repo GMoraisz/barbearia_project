be032f88c201cea30e7b0b6ee241f20d81981b70
Morais <moraisgabriel923@gmail.com>
2025-01-11 13:02:03 -0300

    saving

--- clientes/models.py
+++ clientes/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
-    telefone = models.CharField(max_length=15, unique=True)
+    telefone = models.CharField(
+        max_length=15,
+        unique=True,
+        validators=[RegexValidator(r'^\d{10,15}$', 'Digite apenas números válidos no telefone.')],
+    )
     data_cadastro = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.nome
+        return f"{self.nome} - {self.telefone}"
--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Cliente
+from django.core.validators import RegexValidator
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
-        fields = ['id', 'nome', 'telefone', 'data_cadastro']
+        fields = '__all__'
+
+    telefone = serializers.CharField(
+        max_length=15,
+        validators=[RegexValidator(r'^\d{10,15}$', 'Digite apenas números válidos no telefone.')]
+    )
